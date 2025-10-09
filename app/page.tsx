adafd0263f0f7f5a5b212366ee076b8cc25d363e
rashidmvk <rashidmvk@gmail.com>
2025-10-10 02:43:09 +0300

    Deploy Next.js static site to GitHub Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import { CountdownTimer } from '@/components/CountdownTimer';
 import styles from './page.module.css';
 
 export default function Home() {
